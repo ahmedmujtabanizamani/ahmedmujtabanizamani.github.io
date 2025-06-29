6fdbadd5a51169b4a85de9848cb6da33a5ac0a24
Ahmed Mujtaba Nizamani <mujtaba_niz@live.com>
2025-06-29 21:24:35 +0500

    re engineered the script and added new functionality for non visible link scrape

--- publicscripts/Google Scrapper Web View.user.js
+++ publicscripts/Google Scrapper Web View.user.js
@@ -11,69 +11,121 @@
 (function() {
     'use strict';
     // fix browser view for mobile
-    
+    let str = "test";
     let mapEle = document.querySelector("#rhs");
     let mapPoints = document.querySelectorAll('[data-lat]');
     if(mapPoints.length > 0 && mapEle){
-      // hide map
-      mapEle.style.display = "none";
-      // mobile friendly CSS
-      document.querySelector("#rcnt").style.cssText = "display:block; min-width: auto; width: 100%";
-      document.querySelector("#searchform").style.cssText = "display:block; min-width: auto; width: 100%; overflow: scroll";
-      // hide profile pic area
-      document.querySelector("#searchform form").nextElementSibling.style.display = "none";
-
-      // Create a new meta element
-      var meta = document.createElement('meta');
-      // Set the name and content attributes for the viewport meta tag
-      meta.setAttribute('name', 'viewport');
-      meta.setAttribute('content', 'width=device-width, initial-scale=1;user-scalable=no;user-scalable=0;');
-      // Append the meta element to the head section of the document
-      document.head.appendChild(meta);
+        // hide map
+        mapEle.style.display = "none";
+        // mobile friendly CSS
+        document.querySelector("#rcnt").style.cssText = "display:block; min-width: auto; width: 100%";
+        document.querySelector("#searchform").style.cssText = "display:block; min-width: auto; width: 100%; overflow: scroll";
+        // hide profile pic area
+        document.querySelector("#searchform form").nextElementSibling.style.display = "none";
+        // Create a new meta element
+        var meta = document.createElement('meta');
+        // Set the name and content attributes for the viewport meta tag
+        meta.setAttribute('name', 'viewport');
+        meta.setAttribute('content', 'width=device-width, initial-scale=1;user-scalable=no;user-scalable=0;');
+        // Append the meta element to the head section of the document
+        document.head.appendChild(meta);
     }
-    
 
+    async function sleep(delay){
+        return new Promise((resolve) => setTimeout(resolve, delay));
+    }
     // Your code here...
     async function extractData(){
-        let str = "";
+        // reset data for "str" variable so it contain new data from new scrape
+        str = "";
+        // Page number
         let pageNum = document.querySelectorAll('[aria-label="Local results pagination"]')[0].querySelectorAll(".YyVfkd ")[0].innerText;
+        // Index for each entry in the page
         let indexNum = 1;
         // Entry container
         let entries = document.querySelectorAll('.uMdZh');
-        entries.forEach(x=>{
+        document.querySelector('#copybtn').style.display = "none";
+        // show/reset progress report
+        document.querySelector('#progress-element').style.display = "block";
+        document.querySelector('#progress-element').innerHTML = "Progressing!"
+        // loop through entries
+        for( let i = 0; i < entries.length; i++){
+            let x = entries[i];
+            // do not consider sponsored entries
             let isSponsored = x.parentElement.getAttribute("data-is-ad") == 1;
+            // Serial number for entries like a custom index.
             let SNum = pageNum + "." + indexNum;
+            // other fields to scrape from search entries
             let entryLinks = x.querySelectorAll('a[aria-describedby]');
             let entryLink = "NA";
             let dirLink = "NA";
-            let entryName = x.querySelectorAll('[role=heading]')[0].innerText;
+            let entryNameEle = x.querySelectorAll('[role=heading]')[0];
+            let entryName = entryNameEle.innerText;
+            let newDynamicLinkElement = [];
 
+            // exempt sponsored entries only
             if(!isSponsored){
-
                 // Website link
-                entryLinks.forEach(y=>{
-                    if(!y.getAttribute("data-url")){
+                for(let j = 0; j < entryLinks.length; j++){
+                    if(!entryLinks[j].getAttribute("data-url")){
                         //console.log("this is it" + y.getAttribute("href"));
-                        entryLink = y.getAttribute("href");
+                        entryLink = entryLinks[j].getAttribute("href");
                     }else{
                         //dirLink = "[Dir](https://www.google.com" + y.getAttribute("href") + ")";
                     }
-                });
-				if(entryLink != "NA"){
-					 str += "- " + SNum + "  -  " + entryName + "  -  " + entryLink + "\n";
-				}
+                }
+                // if Website link is not available click on item, so it load the detail
+                if(entryLink == "NA"){
+                    entryNameEle.click();
+                    // wait so data get loaded
+                    await sleep(3000);
+                    // locate Website link now in new loaded data
+                    newDynamicLinkElement = document.querySelectorAll('path[d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM4 12c0-.61.08-1.21.21-1.78L8.99 15v1c0 1.1.9 2 2 2v1.93C7.06 19.43 4 16.07 4 12zm13.89 5.4c-.26-.81-1-1.4-1.9-1.4h-1v-3c0-.55-.45-1-1-1h-6v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41C17.92 5.77 20 8.65 20 12c0 2.08-.81 3.98-2.11 5.4z"]');
+                }
+                // if new data contain some link consider it.
+                if(newDynamicLinkElement.length > 0){
+                    entryLink = newDynamicLinkElement[0].closest('a').href;
+                    // progress report
+                    document.querySelector('#progress-element').innerHTML = document.querySelector('#progress-element').innerHTML + "<br>" + "- " + SNum + "  -  " + entryName + "  -  " + entryLink;
+                    //console.log(entryLink + " - " + entryName);
+                }
+                // exempt non website results
+                if(entryLink != "NA"){
+                    str += "- " + SNum + "  -  " + entryName + "  -  " + entryLink + "\n";
+                }
                 indexNum++;
             }
-        });
+        }
+        // Copy Scaped data button
+        document.querySelector('#copybtn').style.display = "block";
+        alert("please copy now!");
+        // Hide progress report
+        document.querySelector('#progress-element').style.display = "none";
+    }
+    // copy to clipboard does not work unless user click and generate the event manually
+    async function copyToClipboard(){
         await navigator.clipboard.writeText(str);
         alert("copied");
+        document.querySelector('#progress-element').style.display = "none";
     }
-
-    // trigger button
+    // New Elements to generate and embbed in original page
+    // scrape trigger button
     let btn = document.createElement("button");
     btn.innerText = "Click Me Web";
     btn.style.cssText = "color:white; background-color: blue; padding: 20px";
     document.body.appendChild(btn);
-
     btn.addEventListener("click", extractData);
+    // copy button
+    let copybtn = document.createElement("button");
+    copybtn.innerText = "Copy";
+    copybtn.setAttribute("id", "copybtn");
+    copybtn.style.cssText = "display:none;color:white; background-color: green; padding: 10px";
+    document.body.appendChild(copybtn);
+    copybtn.addEventListener("click", copyToClipboard);
+    // Progress Element
+    let progressEle = document.createElement("div");
+    progressEle.setAttribute("id", "progress-element");
+    progressEle.innerHTML = "Progressing!";
+    document.body.appendChild(progressEle);
+    progressEle.style.cssText = "display: none; position: absolute; width: 80%; height: 80%; left: calc(10% - 50px); top: calc(10% - 50px); opacity: 0.7; border-radius: 50px; color: lightgreen; background-color: black; padding: 50px; z-index: 10000";
 })();

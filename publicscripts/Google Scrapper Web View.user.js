a54e9e86161300008a3740292b36f4ff91fe16e6
Ahmed Mujtaba Nizamani <mujtaba_niz@live.com>
2025-06-29 23:48:07 +0500

    css fixes and delay changed

--- publicscripts/Google Scrapper Web View.user.js
+++ publicscripts/Google Scrapper Web View.user.js
@@ -78,7 +78,7 @@
                 if(entryLink == "NA"){
                     entryNameEle.click();
                     // wait so data get loaded
-                    await sleep(3000);
+                    await sleep(4000);
                     // locate Website link now in new loaded data
                     newDynamicLinkElement = document.querySelectorAll('path[d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM4 12c0-.61.08-1.21.21-1.78L8.99 15v1c0 1.1.9 2 2 2v1.93C7.06 19.43 4 16.07 4 12zm13.89 5.4c-.26-.81-1-1.4-1.9-1.4h-1v-3c0-.55-.45-1-1-1h-6v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41C17.92 5.77 20 8.65 20 12c0 2.08-.81 3.98-2.11 5.4z"]');
                 }
@@ -127,5 +127,5 @@
     progressEle.setAttribute("id", "progress-element");
     progressEle.innerHTML = "Progressing!";
     document.body.appendChild(progressEle);
-    progressEle.style.cssText = "display: none; position: absolute; width: 80%; height: 80%; left: calc(10% - 50px); top: calc(10% - 50px); opacity: 0.7; border-radius: 50px; color: lightgreen; background-color: black; padding: 50px; z-index: 10000";
+    progressEle.style.cssText = "overflow: auto; display: none; position: absolute; width: 80%; height: 80%; left: calc(10% - 50px); top: calc(10% - 50px); opacity: 0.7; border-radius: 50px; color: lightgreen; background-color: black; padding: 50px; z-index: 10000";
 })();
